49db023ef86ee133ccb1d94b102a8a8a0c3376d9
Michal Malek <michal.malek98@wp.pl>
2023-05-24 20:52:41 +0200

    init

---
+++ src/test/java/com/example/app/AppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class AppApplicationTests {
+
+    @Test
+    void exampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
